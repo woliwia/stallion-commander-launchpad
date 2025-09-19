9a1364f79f2bb609e91d5c0dcd0b37614d06ee47
eadvientova <eadviento.va@gmail.com>
2025-09-19 23:24:28 +0800

    Swap header logo asset to transparent wordmark

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className="flex items-center justify-center">
           <Link to="/" className="transition-all duration-300 hover:scale-105">
             <img
-              src="/lovable-uploads/9aebaefa-86d2-491f-9e62-42000f1abcef.png"
+              src="/lovable-uploads/57dcb4da-4530-45f2-914e-0725921540d3.png"
               alt="Stallion Commander Logo"
               className="h-5 w-auto max-w-full sm:h-7 md:h-9 lg:h-10"
               loading="eager"
